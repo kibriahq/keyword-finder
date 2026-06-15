61a81088cc8c7e788bada4e468325afa86c006c8
Md Kibria <78228935+kibriahq@users.noreply.github.com>
2026-06-15 11:19:46 +0600

    Update GitHub link in Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
                     <span className="ml-3 text-xl">KeywordFinder</span>
                 </Link>
                 <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2024 Keyword Finder —
-                    <a href="https://github.com/md-kibria" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@md-kibria</a>
+                    <a href="https://github.com/kibriahq" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@kibria</a>
                 </p>
                 <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
                     <a className="text-gray-500">
